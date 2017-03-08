609dfce0d903d1945f1188a88e31b74b742b3d04
patrick kettner <patrickkettner@gmail.com>
2017-03-08 10:48:35 -0800

    Support newer emojis
    Closes #1.

--- tests/tests.js
+++ tests/tests.js
@@ -9,6 +9,9 @@ test(t => {
 	t.true(
 		require('../Binary_Property/Emoji.js') instanceof regenerate
 	);
+	t.true(
+		require('../Binary_Property/Emoji.js').toRegExp().test('\u{1F921}') // U+1F921 CLOWN FACE
+	);
 	t.throws(
 		() => require('../Invalid_Property/X.js'),
 		Error
